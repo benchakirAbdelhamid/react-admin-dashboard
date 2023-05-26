be7f7514d5cf410194b5c63645c64400c59806bb
Software Developer (Full Stack & Web Development) ' React Laravel MongoDB Developer <100100319+benchakirAbdelhamid@users.noreply.github.com>
2023-05-26 11:49:42 +0100

    Update Ticket.js
    add modalBox , in order to verify ticket user information

--- src/scenes/ticket/Ticket.js
+++ src/scenes/ticket/Ticket.js
@@ -1,114 +1,208 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { Box } from '@mui/material';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Backdrop from "@mui/material/Backdrop";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
+const useStyles = makeStyles((theme) => ({
+  button: {
+    backgroundColor: "#7a0ec2cc",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#60079cc7",
+    },
   },
 }));
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  borderRadius: "10px",
+  boxShadow: 100,
+  p: 4,
+};
 
 const Ticket = () => {
-  const state = useSelector((state) => state.topBar);
+  const classes = useStyles();
+  // modal
+  const [open, setOpen] = React.useState(false);
+  const [idUserDeleted, setIdUserDeleted] = React.useState(false);
+  const handleOpen = (id) => {
+    setOpen(true);
+    // console.log('==>',id)
+    setIdUserDeleted(id);
+  };
+  const handleClose = () => setOpen(false);
 
-  const [stateTicket , setstateTicket] = useState(
-    [
-      {
-        id : '1',
-        date:'test date',
-        packId:'test packId',
-        eventId:'test eventId',
-        userId:'test userId',
-        status:'test status',
-        note:'test note',
-      },
-      {
-        id : '2',
-        date:'test date',
-        packId:'test packId',
-        eventId:'test eventId',
-        userId:'test userId',
-        status:'test status',
-        note:'test note',
-      },
-    
-    ]
-  )
+  const [stateTicket, setstateTicket] = useState([
+    {
+      id: "1",
+      date: "test date",
+      packId: "test packId",
+      eventId: "test eventId",
+      userId: "test userId",
+      status: "test status",
+      note: "test note",
+    },
+    {
+      id: "2",
+      date: "test date",
+      packId: "test packId",
+      eventId: "test eventId",
+      userId: "test userId",
+      status: "test status",
+      note: "test note",
+    },
+  ]);
 
-  useEffect(()=>{
-    // console.log(state)
-    // state.organizers.map(itemEvent=>{
-    //   console.log(itemEvent)
-    // })
-    
-      })
-  
-    return (
-      <Box m="20px">
-      <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 700 }} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>date</StyledTableCell>
-                <StyledTableCell align="right">eventId</StyledTableCell>
-                <StyledTableCell align="right">note</StyledTableCell>
-                <StyledTableCell align="right">packId</StyledTableCell>
-                <StyledTableCell align="right">status</StyledTableCell>
-                <StyledTableCell align="right">userId</StyledTableCell>
-                <StyledTableCell align="right">Delete</StyledTableCell>
-                <StyledTableCell align="right">Update</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {stateTicket.map((row) => (
-                <StyledTableRow key={row.id}>
-                  <StyledTableCell component="th" scope="row">
-                    {row.date}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{row.eventId}</StyledTableCell>
-                  <StyledTableCell align="right">{row.note}</StyledTableCell>
-                  <StyledTableCell align="right">{row.packId}</StyledTableCell>
-                  <StyledTableCell align="right">{row.status}</StyledTableCell>
-                  <StyledTableCell align="right">{row.userId}</StyledTableCell>
-                    <StyledTableCell align="right">
-                  <Button variant="contained">Delete</Button>
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                  <Button variant="contained">Update</Button>
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))} 
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
-     
+  const handleInputChange = (e, id, field) => {
+    const updatedTableData = stateTicket.map((item) =>
+      item.id === id ? { ...item, [field]: e.target.value } : item
     );
-}
+    setstateTicket(updatedTableData);
+  };
+
+  const handleDeleteUser = () => {
+    // console.log('deleted')
+    console.log("===> deleted ===> ", idUserDeleted);
+    setOpen(false);
+  };
+
+  const handleNewDataUpdated = (id) => {
+    // console.log(id)
+    stateTicket.forEach((itemData) => {
+      if (itemData.id == id) {
+        console.log(itemData);
+      }
+    });
+  };
+
+  return (
+    <Box m="20px">
+      <div class="overflow-x-scroll">
+      <table id="customers">
+        <thead>
+          <tr>
+            <th>date</th>
+            <th>eventId</th>
+            <th>note</th>
+            <th>packId</th>
+            <th>status</th>
+            <th>userId</th>
+            <th>Delete</th>
+            <th>Update</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stateTicket.map((item) => (
+            <tr key={item.id}>
+              <td component="th" scope="row">
+                <input
+                  type="text"
+                  value={item.date}
+                  onChange={(e) => handleInputChange(e, item.id, "date")}
+                />
+              </td>
+              <td component="th" scope="row">
+                <input
+                  type="text"
+                  value={item.eventId}
+                  onChange={(e) => handleInputChange(e, item.id, "eventId")}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  value={item.note}
+                  onChange={(e) => handleInputChange(e, item.id, "note")}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  value={item.packId}
+                  onChange={(e) => handleInputChange(e, item.id, "packId")}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  value={item.status}
+                  onChange={(e) => handleInputChange(e, item.id, "status")}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  value={item.userId}
+                  onChange={(e) => handleInputChange(e, item.id, "userId")}
+                />
+              </td>
+
+              <td>
+                <Button
+                  className={classes.button}
+                  onClick={(e) => handleOpen(item.id)}
+                >
+                  Delete
+                </Button>
+              </td>
+              <td>
+                <Button
+                  className={classes.button}
+                  onClick={(e) => handleNewDataUpdated(item.id)}
+                >
+                  Update
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+     
+
+      {/* modal */}
+      <div>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <Typography
+                id="transition-modal-title"
+                variant="h6"
+                component="h2"
+              >
+                Are you sure you want to delete this organizer !
+              </Typography>
+              <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                <Button onClick={handleDeleteUser} className={classes.button}>
+                  Yes
+                </Button>
+                <Button onClick={handleClose} className={classes.button}>
+                  no
+                </Button>
+              </Typography>
+            </Box>
+          </Fade>
+        </Modal>
+      </div>
+      {/* modal */}
+    </Box>
+  );
+};
 
 export default Ticket;
